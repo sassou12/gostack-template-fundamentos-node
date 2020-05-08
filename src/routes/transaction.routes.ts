2b6657f2a10c20656c5e05555f360e942669377e
Heider Sassou <sassou12@gmail.com>
2020-05-07 23:35:29 -0300

    Separação das responsabilidades entre as routas e services, passando a regra de negócio da valição de um transação "outcome" para o service

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,16 +1,17 @@
 import { Router } from 'express';
 
 import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import CreateTransactionService from '../services/CreateTransactionService';
 
 const transactionRouter = Router();
 
 const transactionsRepository = new TransactionsRepository();
 
-transactionRouter.get('/', (request, response) => {
+transactionRouter.get('/', (_, response) => {
   try {
     const transactions = transactionsRepository.all();
     const balance = transactionsRepository.getBalance();
+
     return response.json({ transactions, balance });
   } catch (err) {
     return response.status(400).json({ error: err.message });
@@ -21,18 +22,9 @@ transactionRouter.post('/', (request, response) => {
   try {
     const { title, value, type } = request.body;
 
-    if (type === 'outcome') {
-      const balance = transactionsRepository.getBalance();
-      if (balance.total < value) {
-        return response.status(400).json({ error: 'insufficient funds' });
-      }
-    }
+    const service = new CreateTransactionService(transactionsRepository);
+    const transaction = service.execute({ title, value, type });
 
-    const transaction = transactionsRepository.create({
-      title,
-      value,
-      type,
-    });
     return response.json(transaction);
   } catch (err) {
     return response.status(400).json({ error: err.message });
